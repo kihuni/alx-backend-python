3305f5f6c7abaa6fa7ebd132e63aea538075d073
stephen kihuni <stephenkihuni55@gmail.com>
2023-01-06 03:39:03 +0300

    added a basic documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-""" Basic type-annotated function """
+""" Basic annotations add """
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
+    """ Returns sum of two floats """
     return a + b
