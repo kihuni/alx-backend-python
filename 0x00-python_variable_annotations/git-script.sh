809a73351cc2357431e508ba4024b4d40ef2334b
stephen kihuni <stephenkihuni55@gmail.com>
2023-01-06 04:53:12 +0300

    adding python variable task

--- 0x00-python_variable_annotations/git-script.sh
+++ 0x00-python_variable_annotations/git-script.sh
@@ -1,12 +1,16 @@
-echo "adding file to git hub"
+echo "adding file to Staging area"
 
-sleep 2s
+sleep 4s
 
 git add .
+
+sleep 4
 
 echo "commiting changes"
 
 git commit -m "adding python variable task"
+
+sleep 6
 
 echo "pushing to github"
 
