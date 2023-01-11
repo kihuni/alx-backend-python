ae79fe8649b8f98759564e7ec55c1f22e95c27ef
stephen kihuni <stephenkihuni55@gmail.com>
2023-01-11 06:14:05 +0300

    added the remaining task

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Awaits for a random delay between 0 and max_delay
-
+    waits for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -1,1 +1,2 @@
-Async/Await in python
+<center><h1>Python - Async/Await</h1></center>
+
